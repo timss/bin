f8e88f8a3991b5d40fc1cbe298a486ee7ae05d36
timss <timsateroy@gmail.com>
2016-03-24 15:35:33 +0100

    lanuptime.sh: show uptime of hosts in local subnet

---
+++ lanuptime.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# lanuptime.sh:
+# show uptime of hosts in local subnet
+
+set -e
+
+skip=${1:-""}
+default_if=$(ip route list | awk '/^default/ {print $5}')
+subnet=$(ip -o -f inet addr show $default_if | awk '{print $4}')
+hosts=$(nmap -sn $subnet | awk -F '[ ().]' '/for [a-z]+/ {print $5}' | grep -v $skip)
+
+for host in $hosts; do
+    echo -n "$host: " && ssh $host uptime | awk -F '[ , ]' '{print $4" "$5}'
+done
